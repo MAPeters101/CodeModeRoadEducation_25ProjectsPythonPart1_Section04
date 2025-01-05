b5b8af0982860f6704547e654ebd1a996e362ece
Mark Peters <mapeters101@earthlink.net>
2025-01-05 15:13:58 -0500

    Work on lesson019 to 3:50.

--- lesson020.py
+++ lesson020.py
@@ -3,9 +3,17 @@ answer = random.randint(1, 100)
 
 guess = int(input("Enter an integer from 1 to 100: "))
 
-while guess > 100 or guess < 1:
-    print("Invalid number")
+
+while guess != answer:
+    while guess > 100 or guess < 1:
+        print("Invalid number")
+        guess = int(input("Enter an integer from 1 to 100: "))
+
+    if guess > answer:
+        print("Too high!")
+    elif guess < answer:
+        print("Too low!")
+
     guess = int(input("Enter an integer from 1 to 100: "))
 
-
-
+print("You guessed it!")
