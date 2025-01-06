4c27a610c67406d38ecda5b0ba35d2504be79903
Mark Peters <mapeters101@earthlink.net>
2025-01-05 19:41:41 -0500

    Initial code for assignment10.

---
+++ assignment10.py
@@ -0,0 +1,14 @@
+print("Welcome to the Wall of Zeros And Ones\n")
+#create a for loop that loops 10 times (make sure the iteration variable is called i for the purposes of this project)
+
+
+#INSIDE of the for loop loop, create another for loop that loops 10 times (make sure the iteration variable is NOT called i) ALSO, make sure to copy and paste the two conditionals below to be INSIDE of THIS FOR LOOP
+
+
+if i % 2 == 0:
+  print() #determine what needs to be printed if the current iteration is               even (the row is odd)
+
+if i % 2 != 0:
+  print() #determine what needs to be printed if the current iteration is             odd (the row is even)
+
+#type the line of code that we use to skip to the next row
